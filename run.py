370614ce8e01a784c499aad5458bbea74e051b50
jayeokay <42516242+jayeokay@users.noreply.github.com>
2021-12-07 23:12:51 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,137 +1,34 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
+import sys
+import json
+import os
 
-run_200_300 = pd.read_csv('data/20211026T184846_200-300-iperf.csv')
-run_200_1000 = pd.read_csv('data/20211026T184846_200-1000-iperf.csv')
-run_200_25000 = pd.read_csv('data/20211026T184846_200-25000-iperf.csv')
-run_200_5000 = pd.read_csv('data/20211026T184847_200-5000-iperf.csv')
+sys.path.insert(0, 'src')
+from etl import *
 
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_300['Time'],run_200_300['1->2Bytes'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Bytes per second -- 1->2Bytes (200/300)')
-plt.show()
+def main(targets):
 
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_1000['Time'],run_200_1000['1->2Bytes'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Bytes per second -- 1->2Bytes (200/1000)')
-plt.show()
+    data_config = json.load(open('config/data-params.json'))
 
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_25000['Time'],run_200_25000['1->2Bytes'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Bytes per second -- 1->2Bytes (200/25000)')
-plt.show()
+    if 'data' in targets:
+        latency_data_prep(**data_config)
+        
+    if 'loss' in targets:
+        loss_data_prep(**data_config)
 
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_5000['Time'],run_200_5000['1->2Bytes'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Bytes per second -- 1->2Bytes (200/5000)')
-plt.show()
+    if 'test' in targets:
+        loss_data_prep(**data_config)
+        latency_linear_reg(latency_data_prep(**data_config))
+        decision_tree(latency_data_prep(**data_config))
+        svm(latency_data_prep(**data_config))
+        
+    if 'all' in targets:
+        latency_data_prep(**data_config)
+        loss_data_prep(**data_config)
+        latency_linear_reg(latency_data_prep(**data_config))
+        decision_tree(latency_data_prep(**data_config))
+        svm(latency_data_prep(**data_config))
 
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_300['Time'],run_200_300['2->1Bytes'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Bytes per second -- 2->1Bytes (200/300)')
-plt.show()
+if __name__ == '__main__':
 
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_1000['Time'],run_200_1000['2->1Bytes'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Bytes per second -- 2->1Bytes (200/1000)')
-plt.show()
-
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_25000['Time'],run_200_25000['2->1Bytes'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Bytes per second -- 2->1Bytes (200/25000)')
-plt.show()
-
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_5000['Time'],run_200_5000['2->1Bytes'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Bytes per second -- 2->1Bytes (200/5000)')
-plt.show()
-
-
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_300['Time'],run_200_300['2->1Bytes'])
-plt.plot(run_200_1000['Time'],run_200_1000['2->1Bytes'])
-plt.plot(run_200_25000['Time'],run_200_25000['2->1Bytes'])
-plt.plot(run_200_5000['Time'],run_200_5000['2->1Bytes'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Bytes per second -- 2->1Bytes (200/300)')
-plt.show()
-
-def pckt_count(a):
-    a = a.split(';')
-    a = [int(i) for i in a[:-1]]
-    return len(a)
-
-run_200_300['pckt_ct'] = run_200_300['packet_times'].apply(pckt_count)
-
-
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_300['Time'], run_200_300['pckt_ct'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Packets per second -- 1->2Bytes (200/300)')
-plt.show()
-
-run_200_5000['packet_size_total'] = run_200_5000['1->2Pkts'] + run_200_5000['2->1Pkts']
-run_200_300['packet_size_total'] = run_200_300['1->2Pkts'] + run_200_300['2->1Pkts']
-run_200_1000['packet_size_total'] = run_200_1000['1->2Pkts'] + run_200_1000['2->1Pkts']
-run_200_25000['packet_size_total'] = run_200_25000['1->2Pkts'] + run_200_25000['2->1Pkts']
-plt.show()
-
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_5000['Time'], run_200_5000['packet_size_total'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Packet Length per second -- 1->2Bytes (200/5000)')
-plt.show()
-
-figure(figsize=(12, 6), dpi=80)
-plt.plot(run_200_300['Time'], run_200_300['packet_size_total'])
-plt.xlabel('Time (s)')
-plt.ylabel('Bytes')
-plt.title('Packet Length per second -- 1->2Bytes (200/300)')
-plt.show()
-
-
-ps = []
-for i in run_200_5000.packet_sizes[:10]:
-    a = i.split(';')
-    a = [int(i) for i in a[:-1]]
-    ps = ps + a
-ps = ps[:-1]
-
-pt = []
-for i in run_200_5000.packet_times[:10]:
-    a = i.split(';')
-    a = [int(i) for i in a[:-1]]
-    pt = pt + a
-
- diff_list = []
-for x, y in zip(pt[0::], pt[1::]):
-    diff_list.append(y-x)
-
-pt = diff_list
-
-
-figure(figsize=(12, 6), dpi=80)
-plt.plot(pt)
-plt.show()
-
+    targets = sys.argv[1:]
+    main(targets)
